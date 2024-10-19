541d2fc08eedf47061812156a4ced32dbb7fadd5
Minxster <47479832+Minxster@users.noreply.github.com>
2024-10-19 17:06:32 +0100

    Create plugins.py

---
+++ configuration/plugins.py
@@ -0,0 +1,17 @@
+PLUGINS = [
+    "netbox_topology_views",
+    "netbox_reorder_rack",
+    "netbox_documents",
+]
+
+# PLUGINS_CONFIG = {
+#     "netbox_topology_views": {
+#         # Add any plugin-specific configuration here
+#     },
+#     "netbox_reorder_rack": {
+#         # Add any plugin-specific configuration here
+#     },
+#     "netbox_documents": {
+#         # Add any plugin-specific configuration here
+#     },
+# }
